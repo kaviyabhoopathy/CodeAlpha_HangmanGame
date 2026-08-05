ec845037bc304831b64229f51c92897044ec2f16
kaviyabhoopathy <bpkaviya2008@gmail.com>
2026-08-05 12:20:20 +0530

    Update Hanuman.py

--- Hanuman.py
+++ Hanuman.py
@@ -1,32 +1,23 @@
 import random
-
-
 def hangman():
     words = ["apple", "tiger", "chair", "bread", "cloud"]
     word = random.choice(words)
-
     guessed_letters = []
     attempts = 6
-
     print("=== Hangman Game ===")
-
     while attempts > 0:
         display = ""
-
         for letter in word:
             if letter in guessed_letters:
                 display += letter + " "
             else:
                 display += "_ "
-
         print("\nWord:", display)
-
         if "_" not in display:
             print("Congratulations! You guessed the word:", word)
             break
 
         guess = input("Enter a letter: ").lower()
-
         if guess in guessed_letters:
             print("You already guessed that letter.")
 
